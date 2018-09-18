ca1cf6bb3d9a2927589626cdfe85de86093c5f82
svlitvinenko <sv.litvinenko@outlook.com>
2018-09-18 20:02:01 +0300

    Test pull from Sergei111

--- app/src/main/java/com/welop/mbank/StartActivity.java
+++ app/src/main/java/com/welop/mbank/StartActivity.java
@@ -16,7 +16,7 @@ public class StartActivity extends BaseActivity {
     private Button signUp;
 
     private Button test2;
-    //serget pidorriii
+    //serget pidorriiivvvvvvv
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
