b0c7b3dc42a801c942eb3802a47f95b3d84dba00
Oskin Yury <oskinyuri@gmail.com>
2018-09-18 20:04:10 +0300

    Tesr pull request from Yuri #2

--- app/src/main/java/com/welop/mbank/StartActivity.java
+++ app/src/main/java/com/welop/mbank/StartActivity.java
@@ -16,7 +16,7 @@ public class StartActivity extends BaseActivity {
     private Button signUp;
 
     private Button test2;
-    //serget pidorrr
+    //serget pidoras
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
