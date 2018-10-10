5dfef16076f68f77bc70c2789c712b740c336371
svlitvinenko <sv.litvinenko@outlook.com>
2018-10-10 21:19:01 +0300

    Edited cards

--- app/src/main/java/com/welop/mbank/adapters/TransactionRecyclerAdapter.java
+++ app/src/main/java/com/welop/mbank/adapters/TransactionRecyclerAdapter.java
@@ -36,7 +36,7 @@ public class TransactionRecyclerAdapter extends RecyclerView.Adapter<Transaction
 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_transaction, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_transaction, parent, false);
         ViewHolder viewHolder = new ViewHolder(v);
         return viewHolder;
     }
